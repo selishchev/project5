8a4328b06ee3db1815bd037d203aa2dea9968afe
selishchev <selishev2010@gmail.com>
2018-11-12 01:10:06 +0300

    final program

--- project5.py
+++ project5.py
@@ -5,24 +5,28 @@
 и выводит количество пар по этому предмету на данной неделе
 """
 
+import sys
+
 amount = 0
 print('Список предметов: Математический анализ, Программирование, Физическая культура, Английский язык,\n'
       'Основы веб-программирования, Микроэкономика, История, Информатика, Линейная алгебра')
-week = input('Введите неделю("четная" или "нечетная"): ').lower()
+week = input('Введите номер недели(1 - четная или 2 - нечетная): ')
+try:
+    int_week = int(week)
+except ValueError:
+    print('Значение {} не является целым числом'.format(week))
+    sys.exit()
 subject = input('Введите полное название предмета из списка предметов: ').lower()
-if week == 'четная':
+if int_week == 1:
     with open('table1.txt') as f_in:
         for line in f_in:
             new_line = line.lower().strip()
             if new_line == subject:
                 amount += 1
-elif week == 'нечетная':
+elif int_week == 2:
     with open('table2.txt') as f_in:
         for line in f_in:
             new_line = line.lower().strip()
             if new_line == subject:
                 amount += 1
-else:
-    while week != 'четная' or week != 'нечетная':
-        week = input('Введите неделю("четная" или "нечетная"): ').lower()
-print('Количетсво пар по предмету {} на данной неделе: {}'.format(subject, amount))
+print('Количетсво пар по предмету "{}" на данной неделе: {}'.format(subject, amount))
